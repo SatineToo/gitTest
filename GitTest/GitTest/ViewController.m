d31310b9a6e1e3566639da1f51983512e109440e
Bruno.ma <bruno.ma@coupang.com>
2016-05-02 16:59:42 +0800

    create project

---
+++ GitTest/GitTest/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  GitTest
+//
+//  Created by Bruno.ma on 4/26/16.
+//  Copyright © 2016 Bruno.ma. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    UIButton * btn = [[UIButton alloc]initWithFrame:CGRectMake(100, 100, 100, 100)];
+    btn.backgroundColor =[UIColor redColor];
+    [self.view addSubview:btn];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
